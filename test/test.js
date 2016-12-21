1a1602cfc81bd33be4fe8e7f63f72c2eb96e5186
Peter Springer <peterspringer829@gmail.com>
2016-12-21 12:00:44 -0700

    wire up testing suite

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+
+// test suite is wired up but is not testing anything
+
+  describe('the server exists', function(){
+    context('find server', function(){
+      it('should return true', function(){
+        assert.equal();
+      });
+    });
+  });
